587d785dbedfeeb9df06ac2001995caf83fc7e46
Dan Horrigan <dan@dhorrigan.com>
2010-10-22 09:02:05 -0400

    Adding the application classes folder. Also adding a basic config file.

---
+++ carbon/application/config/config.php
@@ -0,0 +1,24 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Carbon
+ *
+ * Carbon is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package		Carbon
+ * @version		1.0
+ * @author		Dan Horrigan <http://dhorrigan.com>
+ * @license		Apache License v2.0
+ * @copyright	2010 Dan Horrigan
+ */
+
+return array(
+	
+	/**
+	 * index_file - The name of the main bootstrap file.
+	 *
+	 * Set this to FALSE or remove if you using mod_rewrite.
+	 */
+	'index_file'	=> 'index.php',
+);
+
+/* End of file config.php */
