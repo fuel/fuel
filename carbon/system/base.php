2239b32de79ee3420e73d72938dedb70cbc67839
Dan Horrigan <dan@dhorrigan.com>
2010-10-22 09:01:22 -0400

    Adding base.php.  Contains low level functions used by the framework.

---
+++ carbon/system/base.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// Get the start time and memory for use later
+defined('CARBON_START_TIME') OR define('CARBON_START_TIME', microtime(TRUE));
+defined('CARBON_START_MEM') OR define('CARBON_START_MEM', memory_get_usage());
+
+/* End of file base.php */
