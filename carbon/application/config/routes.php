819b35b9b3d075b315d817c05cf767f39b2ed071
Dan Horrigan <dan@dhorrigan.com>
2010-10-22 20:18:47 -0400

    Routes config file

---
+++ carbon/application/config/routes.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Carbon
+ *
+ * Carbon is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package		Carbon
+ * @version		1.0
+ * @author		Dan Horrigan <http://dhorrigan.com>
+ * @license		Apache License v2.0
+ * @copyright	2010 Dan Horrigan
+ */
+
+return array(
+	'default'	=> 'welcome',
+);
+
+/* End of file routes.php */
