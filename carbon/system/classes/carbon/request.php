9cbc65d5ddd760551e371a56be37b0e7b43c12fc
Dan Horrigan <dan@dhorrigan.com>
2010-10-23 13:43:00 -0400

    Heavily modified the Request class to use the new URI and Input classes.

--- carbon/system/classes/carbon/request.php
+++ carbon/system/classes/carbon/request.php
@@ -71,173 +71,39 @@ class Carbon_Request {
 	public static $instance = FALSE;
 	
 	/**
-	 * @var	object	Holds the current request being executed
+	 * @var	object	Holds the global request instance
 	 */
-	public static $current = FALSE;
+	public static $active = FALSE;
 
 	/**
-	 * @var	string	The request method
+	 * Returns the a Request object singleton
+	 *
+	 * @static
+	 * @access	public
+	 * @return	object
 	 */
-	public static $method = 'GET';
-
-	/**
-	 * @var	string	The request protocol
-	 */
-	public static $protocol = 'http';
-
-	/**
-	 * @var	string	Referring URL
-	 */
-	public static $referrer;
-
-	/**
-	 * @var	string	The client user agent
-	 */
-	public static $user_agent = '';
-
-	/**
-	 * @var	string	Client IP address
-	 */
-	public static $client_ip = '0.0.0.0';
-
-	/**
-	 * @var	bool	Whether this is an AJAX request
-	 */
-	public static $is_ajax = FALSE;
-
 	public static function instance($uri = NULL)
 	{
-		// Only process the initial request once
 		if ( ! Request::$instance)
 		{
-			// Get the IP address
-			Request::$client_ip = Request::real_ip();
-
-			// Lets get the Request method
-			isset($_SERVER['REQUEST_METHOD']) AND Request::$method = $_SERVER['REQUEST_METHOD'];
-			
-			// Check if the protocol is HTTPS
-			( ! empty($_SERVER['HTTPS'])) AND Request::$protocol = 'https';
-
-			// Check and see if this is an AJAX request
-			if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest')
-			{
-				Request::$is_ajax = TRUE;
-			}
-
-			// Get the referer
-			isset($_SERVER['HTTP_REFERER']) AND Request::$referrer = $_SERVER['HTTP_REFERER'];
-
-			// Get the user agent
-			isset($_SERVER['HTTP_USER_AGENT']) AND Request::$user_agent = $_SERVER['HTTP_USER_AGENT'];
-
-			// We manually parse the php input into $_POST if we aren't using GET or POST
-			if (Request::$method !== 'GET' AND Request::$method !== 'POST')
-			{
-				parse_str(file_get_contents('php://input'), $_POST);
-			}
-
-			if ($uri === NULL)
-			{
-				$uri = Request::get_uri();
-			}
-
+			Request::$instance = Request::$active = new Request($uri);
 		}
-
-		Request::$instance = Request::$current = new Request($uri);
 
 		return Request::$instance;
 	}
 
-	public static function get_uri()
-	{
-		// We want to use PATH_INFO if we can
-		if ( ! empty($_SERVER['PATH_INFO']))
-		{
-			$uri = $_SERVER['PATH_INFO'];
-		}
-		else
-		{
-			if (isset($_SERVER['REQUEST_URI']))
-			{
-				// Some servers require 'index.php?' as the index page
-				// if we are using mod_rewrite or the server does not require
-				// the question mark, then parse the url.
-				if (Config::get('index_file') != 'index.php?')
-				{
-					$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-				}
-				else
-				{
-					$uri = $_SERVER['REQUEST_URI'];
-				}
-			}
-			else
-			{
-				throw new Carbon_Exception('Unable to detect the URI.');
-			}
-
-			// Remove the base URL from the URI
-			$base_url = parse_url(Config::get('base_url'), PHP_URL_PATH);
-
-			if ($uri != '' AND strpos($uri, $base_url) === 0)
-			{
-				$uri = substr($uri, strlen($base_url));
-			}
-
-			$index_file = Config::get('index_file');
-
-			// If we are using an index file (not mod_rewrite) then remove it
-			if ($index_file AND strpos($uri, $index_file) === 0)
-			{
-				$uri = substr($uri, strlen($index_file));
-			}
-
-			// Lets split the URI up in case it containes a ?.  This would
-			// indecate the server requires 'index.php?' and that mod_rewrite
-			// is not being used.
-			preg_match('#(.*?)\?(.*)#i', $uri, $matches);
-
-			// If there are matches then lets set set everything correctly
-			if ( ! empty($matches))
-			{
-				$uri = $matches[1];
-				$_SERVER['QUERY_STRING'] = $matches[2];
-				parse_str($matches[2], $_GET);
-			}
-		}
-
-		// Get rid of double slashes
-		$uri = str_replace('//', '/', $uri);
-
-		// Get rid of any dot paths.
-		$uri = str_replace('../', '', $uri);
-
-		return $uri;
-	}
-
 	/**
-	 * Get the real ip address of the user.  Even if they are using a proxy.
+	 * Returns the active request
 	 *
 	 * @static
 	 * @access	public
-	 * @return	string
+	 * @return	object
 	 */
-	public static function real_ip()
+	public static function active()
 	{
-		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-		{
-			return $_SERVER['HTTP_X_FORWARDED_FOR'];
-		}
-		elseif (isset($_SERVER['HTTP_CLIENT_IP']))
-		{
-			return $_SERVER['HTTP_CLIENT_IP'];
-		}
-		elseif (isset($_SERVER['REMOTE_ADDR']))
-		{
-			return $_SERVER['REMOTE_ADDR'];
-		}
+		return Request::$active;
 	}
+
 
 	/**
 	 * Generates a new request.  This is used for HMVC.
@@ -257,44 +123,29 @@ class Carbon_Request {
 	public $output = NULL;
 
 	/**
-	 * @var	string	The Request's URI
+	 * @var	object	The request's URI object
 	 */
 	public $uri = '';
 
 	/**
-	 * @var	string	The Request's Routed URI
+	 * @var	string	The request's controller
 	 */
-	public $routed_uri = '';
-
-	/**
-	 * @var	array	The URI segments
-	 */
-	public $segments = array();
-
-	/**
-	 * @var	array	The routed URI segments
-	 */
-	public $routed_segments = array();
-
 	public $controller = '';
-	
+
+	/**
+	 * @var	string	The request's action
+	 */
 	public $action = 'index';
 
 	public function __construct($uri)
 	{
-		$uri = trim($uri, '/');
-		
-		$this->uri = $uri;
-		$this->segments = explode('/', $uri);
+		$this->uri = new URI($uri);
 	}
 
 	public function execute()
 	{
 		// TODO: Write the Route class and parse the routes.
-		$this->routed_uri = 'welcome/index';
-		$this->routed_segments = explode('/', $this->routed_uri);
-
-		list($controller, $action) = array_pad($this->routed_segments, 2, FALSE);
+		list($controller, $action) = array_pad($this->uri->segments, 2, FALSE);
 		
 		$controller AND $this->controller = $controller;
 		$action AND $this->action = $action;
@@ -308,15 +159,6 @@ class Carbon_Request {
 		return $this;
 	}
 
-	public function segment($segment_num)
-	{
-		if ( ! isset($this->segments[$segment_num - 1]))
-		{
-			throw new Carbon_Exception('Invalid segment number \''.$segment_num.'\'.');
-		}
-
-		return $this->segments[$segment_num - 1];
-	}
 }
 
 /* End of file request.php */
