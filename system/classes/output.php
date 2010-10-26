2674ef39f882bceb897b87ce29c1ebdd3e0d7c98
Dan Horrigan <dan@dhorrigan.com>
2010-10-26 16:10:19 -0400

    Adding an Output class.

---
+++ system/classes/output.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Fuel
+ *
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package		Fuel
+ * @version		1.0
+ * @author		Dan Horrigan <http://dhorrigan.com>
+ * @license		Apache License v2.0
+ * @copyright	2010 Dan Horrigan
+ */
+
+class Output extends Fuel_Output { }
+
+/* End of file output.php */
