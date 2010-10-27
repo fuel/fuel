5fe4608b0931c8dc30d6c441b5f86083d48de821
Phil Sturgeon <email@philsturgeon.co.uk>
2010-10-27 09:31:00 +0100

    Added View library, credit to Kohana for this great piece of code.

---
+++ system/classes/view.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Fuel
+ *
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package		Fuel
+ * @version		1.0
+ * @author		Fuel Development Team
+ * @license		MIT License
+ * @copyright	2010 Dan Horrigan
+ * @link		http://fuelphp.com
+ */
+
+class View extends Fuel_View { }
+
+/* End of file fuel.php */
