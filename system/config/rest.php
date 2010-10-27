3cbd4242559dad972417155d09cb32c26e9078d2
Phil Sturgeon <email@philsturgeon.co.uk>
2010-10-27 09:50:30 +0100

    Added Controller_Rest for making simple Rest API controllers. Routing is not done yet so HTTP verb-based routes are not implemented, but you can use this syntax: http://d.pr/fKv7

---
+++ system/config/rest.php
@@ -0,0 +1,50 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Fuel
+ *
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package		Fuel
+ * @version		1.0
+ * @author		Dan Horrigan <http://dhorrigan.com>
+ * @license		Apache License v2.0
+ * @copyright	2010 Dan Horrigan
+ */
+
+return array(
+
+	/*
+	| What format should the data be returned in by default?
+	|
+	|	Default: xml
+	|
+	*/
+	'default_format' => 'xml',
+
+	/*
+	| Name for the password protected REST API displayed on login dialogs
+	|
+	|	E.g: My Secret REST API
+	|
+	*/
+	'realm' => 'REST API',
+
+	/*
+	| Is login required and if so, which type of login?
+	|
+	|	'' = no login required, 'basic' = unsecure login, 'digest' = more secure login
+	|
+	*/
+	'auth' => '',
+
+	/*
+	| Array of usernames and passwords for login
+	|
+	|	array('admin' => '1234')
+	|
+	*/
+	'valid_logins' => array('admin' => '1234'),
+
+);
+
+/* End of file config/asset.php */
