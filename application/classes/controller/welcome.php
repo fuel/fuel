dcb4bd1944569c984c7657943a4fff23952ef05e
WanWizard <wanwizard@exitecms.org>
2010-10-29 06:00:46 +0800

    first attempt at a driver based session class

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -1,12 +1,21 @@
 <?php
 
 class Controller_Welcome extends Controller {
-	
+
 	public function action_index()
 	{
-		$this->request->output = 'Hello from the the Welcome controller!';
+		$session = Session::instance();
+		$session->read();
+		$x = $session->get('counter');
+		$x = $x !== FALSE ? ++$x : 0;
+		$session->set('counter', $x);
+		echo "Visit ", $x,"<br>";
+		$session->write();
+//	$session->destroy();
+
+		$this->request->output = '<hr />Hello from the the Welcome controller!';
 	}
-	
+
 	public function action_404()
 	{
 		$this->request->output = '404';
---
+++ system/classes/session.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Fuel
+ *
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package		Fuel
+ * @version		1.0
+ * @author		Fuel Development Team
+ * @license		MIT License
+ * @copyright	2010 Dan Horrigan
+ * @link		http://fuelphp.com
+ */
+
+class Session extends Fuel_Session { }
+
+/* End of file session.php */
