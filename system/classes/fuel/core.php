fe7c662733327d54735fafc564ddeae339e53cb7
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2010-10-31 17:38:16 +0100

    Changed the autoloader to use eval() instead of loading all files which only include an empty class extending the original class in the Fuel core directory.
    Signed-off-by: Jelmer Schreuder <j.schreuder@mijnpraktijk.com>

--- system/classes/fuel/core.php
+++ system/classes/fuel/core.php
@@ -111,6 +111,15 @@ class Fuel_Core {
 			
 			return true;
 		}
+		elseif ($folder == 'classes')
+		{
+			$path = SYSPATH.$folder.DIRECTORY_SEPARATOR.'fuel'.DIRECTORY_SEPARATOR.strtolower($file).'.php';
+			if (is_file($path))
+			{
+				require($path);
+				eval('class '.$called_class.' extends Fuel_'.$called_class.' {}');
+			}
+		}
 
 		// Class is not in the filesystem
 		return false;
@@ -138,6 +147,7 @@ class Fuel_Core {
 				break;
 			}
 		}
+		
 		return $found;
 	}
 
