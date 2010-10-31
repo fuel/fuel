9840b603aca342e01e34709c457df71aaf4ba488
WanWizard <wanwizard@exitecms.org>
2010-10-31 18:21:04 +0800

    added flash key support modified the config system so most options have defaults default the config to cookie sessions updated phpdoc

--- system/config/session.php
+++ system/config/session.php
@@ -13,34 +13,35 @@
  */
 
 /**
- * Config information:
- * - type = cookie
+ * Config information per type:
+ * - type => cookie
  * 		- config array is not used
  *
- * - type = file
+ * - type => file
  * 		- path				path where the session files should be stored.
  * 		- gc_probablility	probability % (between 0 and 100) for garbage collection
  *
- * - type = memcached
+ * - type => memcached
  * 		- servers			array with one or more server definitions, each an array with:
  * 			- host			name or IP address of the host that runs the memcached service
  * 			- port			port memcached listens to
- * 		  	( if not defined, one server will be added as default, host = 127.0.0.1, port = 11211 )
+ * 			- weight		weight of the server in relation to the total
+ * 		( if not defined, one server will be added as default, host = 127.0.0.1, port = 11211 )
  */
 
 return array(
-	'type'						=> 'memcached',					// for now, only 'cookie' and 'file' support
+	'type'						=> 'cookie',				// only 'cookie', 'file' and 'memcached' support
 	'config'					=> array(					// type specific config settings
 									),
-	'match_ip'					=> TRUE,					// check for an IP address match after loading the cookie
-	'match_ua'					=> TRUE,					// check for a user agent match after loading the cookie
-	'cookie_name'				=> 'fuelsession',			// name of the session cookie
-	'cookie_domain' 			=> '',						// cookie domain
-	'cookie_path'				=> '/',						// cookie path
-	'expiration_time'			=> 0,						// cookie expiration time, 0 = until browser close
-	'rotation_time'				=> 300,						// session ID rotation time
-	'flash_id'					=> 'flash',					// default ID for flash variables
-	'flash_auto_expire'			=> FALSE					// if FALSE, expire flash values only after it's used
+	'match_ip'					=> true,					// check for an IP address match after loading the cookie (optional, default = true)
+	'match_ua'					=> true,					// check for a user agent match after loading the cookie (optional, default = true)
+	'cookie_name'				=> 'fuelsession',			// name of the session cookie  (optional, default = 'fuelsession')
+	'cookie_domain' 			=> '',						// cookie domain  (optional, default = '')
+	'cookie_path'				=> '/',						// cookie path  (optional, default = '/')
+	'expiration_time'			=> 0,						// cookie expiration time, 0 = until browser close  (optional, default = 0)
+	'rotation_time'				=> 300,						// session ID rotation time  (optional, default = 300)
+	'flash_id'					=> 'flash',					// default ID for flash variables  (optional, default = 'flash')
+	'flash_auto_expire'			=> false					// if FALSE, expire flash values only after it's used  (optional, default = true)
 );
 
 /* End of file config/session.php */
