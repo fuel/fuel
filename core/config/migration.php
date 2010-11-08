e259d3b81c0677b2e2afdd21aa283875fc2f58eb
Phil Sturgeon <email@philsturgeon.co.uk>
2010-11-08 16:24:06 +0000

    Migrations finished. Example: http://d.pr/9Fsi

---
+++ core/config/migration.php
@@ -0,0 +1,28 @@
+<?php defined('COREPATH') or die('No direct script access.');
+/**
+ * Fuel
+ *
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package		Fuel
+ * @version		1.0
+ * @author		Dan Horrigan <http://dhorrigan.com>
+ * @license		Apache License v2.0
+ * @copyright	2010 Dan Horrigan
+ */
+
+return array(
+
+	/*
+	| Where are these migrations stored?
+	|
+	|	Default: APPPATH.'migrations/'
+	|
+	*/
+	'path' => APPPATH.'migrations/',
+
+	'version' => 1
+
+);
+
+/* End of file config/asset.php */
