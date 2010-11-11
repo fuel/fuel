04f97fdee08df0267e85dc7c7ca2313febe4ce78
Dan Horrigan <dan@dhorrigan.com>
2010-11-11 11:14:38 -0500

    Initial overhaul of the bootstrapping.

--- core/base.php
+++
@@ -1,7 +0,0 @@
-<?php defined('COREPATH') or die('No direct script access.');
-
-// Get the start time and memory for use later
-defined('FUEL_START_TIME') or define('FUEL_START_TIME', microtime(true));
-defined('FUEL_START_MEM') or define('FUEL_START_MEM', memory_get_usage());
-
-/* End of file base.php */
--- index.php
+++ index.php
@@ -1,32 +1,13 @@
 <?php
 /**
- * Fuel
- *
- * Fuel is a fast, lightweight, community driven PHP5 framework.
- *
- * @package		Fuel
- * @version		1.0
- * @author		Fuel Development Team
- * @license		MIT License
- * @copyright	2010 Dan Horrigan
- * @link		http://fuelphp.com
+ * This is the path to the app directory.
  */
+$app_path = './app';
 
 /**
- * Change these paths to point to the correct location.
- * The paths are relative to your index.php
+ * This is the path to the package directory.
  */
-$fuel_paths = array(
-	'app'		=> './app',			// The path to the app folder
-	'packages'	=> './packages',	// The path to the packages folder
-	'core'		=> './core'			// The path to the core folder
-);
-
-/**
- * In a production environment you will want to change these 2 settings.
- */
-error_reporting(E_ALL);
-ini_set('display_errors', true);
+$package_path = './packages';
 
 /**
  * We disable short open tags by default so as to not confuse people.  They
@@ -35,57 +16,29 @@ ini_set('display_errors', true);
 ini_set('short_open_tag', 0);
 
 /**
- * Do we have access to mbstring?
- * We need this in order to work with UTF-8 strings
+ * The apps default timezone
+ * 
+ * @see http://www.php.net/timezones
  */
-define('MBSTRING', function_exists('mb_get_info'));
+date_default_timezone_set('America/New_York');
+
+/**
+ * Define the internal encoding to use.
+ * 
+ * @todo Re-evaluate how to handle this.
+ */
 define('INTERNAL_ENC', 'ISO-8859-1');
 
-// The full path to this file
+/**
+ * Get the current path
+ */
 define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 
-// Loop through the paths and check if they are absolute or relative.
-foreach ($fuel_paths as &$folder)
-{
-	if ( ! is_dir($folder) and is_dir(DOCROOT.$folder))
-	{
-		$folder = DOCROOT.$folder;
-	}
-}
+/**
+ * Boots the system and executes the request.  To change the path to the core,
+ * simply change this require path.
+ */
+require './core/boot.php';
 
-// Define the global path constants
-define('APPPATH', realpath($fuel_paths['app']).DIRECTORY_SEPARATOR);
-define('PKGPATH', realpath($fuel_paths['packages']).DIRECTORY_SEPARATOR);
-define('COREPATH', realpath($fuel_paths['core']).DIRECTORY_SEPARATOR);
-
-// save a bit of memory by unsetting the path array
-unset($fuel_paths);
-
-// Load the base, low-level functions
-require COREPATH.'base.php';
-
-// Load in the core class
-require COREPATH.'classes/fuel/core.php';
-
-// If the Fuel class is overrided in the application folder
-// load that, else load the core class.
-if (is_file(APPPATH.'classes/fuel.php'))
-{
-	require APPPATH.'classes/fuel.php';
-}
-else
-{
-	require COREPATH.'classes/fuel.php';
-}
-
-// Initialize the framework
-// and start buffering the output.
-Fuel::init();
-
-$request = Request::instance();
-$request->execute();
-echo $request->output;
-
-Fuel::finish();
 
 /* End of file index.php */
