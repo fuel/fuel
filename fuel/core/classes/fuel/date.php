1ecf6efe8d8e4a4646b2e1546dd8b81bb8234163
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2010-11-23 23:43:31 +0100

    Removed timezone by numeric input stuff and some comment edits.

--- fuel/core/classes/fuel/date.php
+++ fuel/core/classes/fuel/date.php
@@ -30,7 +30,7 @@ namespace Fuel;
  * Notes:
  * - Always returns Date objects, will accept both Date objects and UNIX timestamps
  * - create_time() uses strptime and has currently a very bad hack to use strtotime for windows servers
- * - Uses strftime formatting for dates http://www.php.net/manual/en/function.strftime.php
+ * - Uses strftime formatting for dates www.php.net/manual/en/function.strftime.php
  */
 class Date {
 
@@ -44,24 +44,14 @@ class Date {
 	protected static $server_gmt_offset = 0;
 
 	/**
-	 * @var string default timezone offset from gmt in hours
+	 * @var string default timezone, must be valid PHP timezone from www.php.net/timezones
 	 */
-	protected static $default_timezone = 'UTC';
+	protected static $default_timezone;
 
 	/**
 	 * @var string default pattern for date output
 	 */
-	protected static $default_pattern = 'human';
-
-	/**
-	 * @var array rewritten timezones array
-	 */
-	protected static $php_timezones = array();
-
-	/**
-	 * @var array Allows for using numeric timezone input
-	 */
-	protected static $offset_to_timezone = array();
+	protected static $default_pattern = 'local';
 
 	/* ---------------------------------------------------------------------------
 	 * DYNAMIC PROPERTIES
@@ -84,20 +74,13 @@ class Date {
 	public static function _init()
 	{
 		static::$server_gmt_offset	= Config::get('server_gmt_offset', 0);
-		static::$default_timezone		= Config::get('default_timezone', 'UTC');
-
-		// Allow the default timezone to be set numericly
-		if ( ! is_string(static::$default_timezone))
-		{
-			static::$default_timezone = static::offset_to_timezone(static::$default_timezone);
-		}
 
 		// Set the default timezone
-		date_default_timezone_set(static::$default_timezone);
+		static::$default_timezone	= date_default_timezone_get();
 
 		// Ugly temporary windows fix because windows doesn't support strptime()
 		// Better fix will accept custom pattern parsing but only parse numeric input on windows servers
-		if ( ! function_exists('strptime'))
+		if ( ! function_exists('strptime') && ! function_exists('Fuel\\strptime'))
 		{
 			function strptime($input, $format)
 			{
@@ -111,11 +94,18 @@ class Date {
 					'tm_sec'	=> date('s', $ts)
 				);
 				// This really is some fugly code, but someone at PHP HQ decided strptime should
-				// output this awfull array instead of a timestamp LIKE EVERYONE ELSE DOES!!!
+				// output this awful array instead of a timestamp LIKE EVERYONE ELSE DOES!!!
 			}
 		}
 	}
 
+	/**
+	 * Create Date object from timestamp, timezone is optional
+	 *
+	 * @param	int		UNIX timestamp from current server
+	 * @param	string	valid PHP timezone from www.php.net/timezones
+	 * @return	Date
+	 */
 	public function factory($timestamp = null, $timezone = null)
 	{
 		$timestamp	= is_null($timestamp) ? time() + static::$server_gmt_offset : $timestamp;
@@ -217,54 +207,8 @@ class Date {
 			}
 		}
 
-		$days_in_month	= array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+		$days_in_month = array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
 		return $days_in_month[$month];
-	}
-
-	/**
-	 * Returns an array of timezones, rewrites build in output to something usable
-	 *
-	 * @return array
-	 */
-	public static function list_timezones()
-	{
-		if ( ! empty(static::$php_timezones))
-		{
-			return static::$php_timezones;
-		}
-		
-		$timezones = \DateTimeZone::listAbbreviations();
-		foreach ($timezones as $area)
-		{
-			foreach ($area as $country)
-			{
-				static::$php_timezones[$country['timezone_id']] = array('offset' => $country['offset'], 'dst' => $country['dst']);
-
-				$offset = (int) ($country['offset'] / 3600);
-				if ( ! array_key_exists($offset, static::$offset_to_timezone) ||
-					 ! array_key_exists((int) $country['dst'], static::$offset_to_timezone[$offset]))
-				{
-					static::$offset_to_timezone[$offset][$country['dst']] = $country['timezone_id'];
-				}
-			}
-		}
-		echo '<pre>';
-		exit(print_r(\DateTimeZone::listAbbreviations()));
-
-		return static::$php_timezones;
-	}
-
-	/**
-	 * Takes an offset in seconds and returns its PHP named timezone
-	 *
-	 * @param int $offset
-	 * @return string
-	 */
-	public static function offset_to_timezone($offset, $dst = false)
-	{
-		static::list_timezones();
-
-		return static::$offset_to_timezone[(int) $offset][(int) $dst];
 	}
 
 	/* ---------------------------------------------------------------------------
@@ -280,16 +224,14 @@ class Date {
 	/**
 	 * Returns the date formatted according to the current locale
 	 * 
-	 * @param	int|Date	time to display
-	 * @param	string		either a named pattern from date config file or a pattern
+	 * @param	string	either a named pattern from date config file or a pattern
 	 * @return	string
 	 */
-	public function format($pattern_key = 'human')
+	public function format($pattern_key = 'local')
 	{
 		Config::load('date', 'date');
 
-		$pattern = Config::get('date.patterns.'.$pattern_key, null);
-		$pattern = ($pattern === null) ? $pattern_key : $pattern;
+		$pattern = Config::get('date.patterns.'.$pattern_key, $pattern_key);
 
 		// Temporarily change timezone when different from default
 		if (static::$default_timezone != $this->timezone)
@@ -312,7 +254,7 @@ class Date {
 	/**
 	 * Returns the internal timestamp
 	 * 
-	 * @return int
+	 * @return	string
 	 */
 	public function get_timestamp()
 	{
@@ -322,9 +264,9 @@ class Date {
 	/**
 	 * Returns the internal timezone
 	 *
-	 * @return string|double
+	 * @return	string
 	 */
-	public function get_timezone($numeric = false)
+	public function get_timezone()
 	{
 		return $this->timezone;
 	}
@@ -332,28 +274,19 @@ class Date {
 	/**
 	 * Change the timezone
 	 *
-	 * @param	string|int	timezone as offset in hours or PHP named
-	 * @param	bool		whether or not DST is active, only used for numeric input
+	 * @param	string	timezone from www.php.net/timezones
 	 */
-	public function set_timezone($timezone, $dst = false)
+	public function set_timezone($timezone)
 	{
-		if (is_string($timezone))
-		{
-			$this->timezone = $timezone;
-		}
-		else
-		{
-			$offset = (int) $timezone;
-			$this->timezone = static::offset_to_timezone($offset, $dst);
-		}
-		
+		$this->timezone = $timezone;
+
 		return $this;
 	}
 	
 	/**
 	 * Allows you to just put the object in a string and get it inserted in the default pattern
 	 *
-	 * @return string
+	 * @return	string
 	 */
 	public function __toString()
 	{
