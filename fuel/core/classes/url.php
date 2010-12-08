22727eaa3c29841a8bd00ae0860ef0df8eb4b7d3
Dan Horrigan <dan@dhorrigan.com>
2010-12-08 15:05:46 -0500

    Fixed a few issues in the Url class and updated everything to our coding standards.

--- fuel/core/classes/url.php
+++ fuel/core/classes/url.php
@@ -13,18 +13,17 @@
 
 namespace Fuel;
 
-class URL {
+use Fuel\Application as App;
+
+class Url {
 
 	/**
-	 * Anchor
-	 *
 	 * Creates an html link
 	 *
-	 * @access	public
-	 * @param	string	The URL
-	 * @param	string	The text value
-	 * @param	array	The attributes array
-	 * @return	string	The html link
+	 * @param	string	the url
+	 * @param	string	the text value
+	 * @param	array	the attributes array
+	 * @return	string	the html link
 	 */
 	public static function anchor($href, $text, $attributes = array())
 	{
@@ -35,25 +34,28 @@ class URL {
 	// --------------------------------------------------------------------
 
 	/**
-	 * Href
+	 * Creates a url with the given uri, including the base url
 	 *
-	 * Creates an html link
-	 *
-	 * @access	public
-	 * @param	string	The URL
+	 * @param	string	the url
+	 * @param	array	some variables for the url
 	 */
-	public static function href($href)
+	public static function href($href, $variables = array())
 	{
 		if ( ! preg_match('#^\w+://# i', $href))
 		{
-			$url = Config::get('base_url');
+			$url = App\Config::get('base_url');
 
-			if (Config::get('index_file'))
+			if (App\Config::get('index_file'))
 			{
-				$url .= Config::get('index_file').'/';
+				$url .= App\Config::get('index_file').'/';
 			}
 
-			$href = $url.$href;
+			$href = $url.ltrim($href, '/');
+		}
+
+		foreach($variables as $key => $val)
+		{
+			$href = str_replace(':'.$key, $val, $href);
 		}
 
 		return $href;
@@ -62,55 +64,45 @@ class URL {
 	// --------------------------------------------------------------------
 
 	/**
-	 * Href
+	 * Gets tje current URL, including the BASE_URL
 	 *
-	 * Creates an html link
-	 *
-	 * @access	public
-	 * @param	string	The URL
+	 * @param	string	the url
 	 */
 	public static function current()
 	{
 		return static::href(Request::active()->uri->uri);
 	}
 
-	// --------------------------------------------------------------------
-
 	/**
-	 * Mailto
+	 * Creates a mailto link.
 	 *
-	 * Creates a mailto link
-	 *
-	 * @access	public
 	 * @param	string	The email address
 	 * @param	string	The text value
-	 * @param 	string	The subject
+	 * @param	string	The subject
 	 * @return	string	The mailto link
 	 */
-	public static function mail_to($email, $text = NULL, $subject = NULL)
+	public static function mail_to($email, $text = NULL, $subject = NULL, $attr = array())
 	{
 		$text or $text = $email;
 
 		$subject and $subject = '?subject='.$subject;
 
-		return '<a href="mailto:'.$email.$subject.'">'.$text.'</a>';
+		return html_tag('a', array(
+			'href' => 'mailto:'.$email.$subject,
+		) + $attr, $text);
 	}
 
-	// --------------------------------------------------------------------------
-
 	/**
-	 * Mailto safe
+	 * Creates a mailto link with Javascript to prevent bots from picking up the
+	 * email address.
 	 *
-	 * Creates a mailto link with Javascript to prevent
-	 * bots from picking up the email address
-	 *
-	 * @access	public
-	 * @param	string	The email address
-	 * @param	string	The text value
-	 * @param 	string	The subject
-	 * @return	string	The javascript code containg email
+	 * @param	string	the email address
+	 * @param	string	the text value
+	 * @param	string	the subject
+	 * @param	array	attributes for the tag
+	 * @return	string	the javascript code containg email
 	 */
-	public static function mail_to_safe($email, $text, $subject = NULL)
+	public static function mail_to_safe($email, $text, $subject = null, $attr = array())
 	{
 		$text or $text = str_replace('@', '[at]', $email);
 
@@ -118,90 +110,87 @@ class URL {
 		
 		$subject and $subject = '?subject='.$subject;
 		
+		$attr = array_to_attr($attr);
+		$attr = ($attr == '' ? '' : ' ').$attr;
+		
 		$output = '<script type="text/javascript">';
 		$output .= 'var user = "'.$email[0].'";';
 		$output .= 'var at = "@";';
 		$output .= 'var server = "'.$email[1].'";';
-		$output .= 'document.write("<a href=" + "mail" + "to:" + user + at + server + "'.$subject.'>'.$text.'</a>");';
+		$output .= "document.write('<a href=\"' + 'mail' + 'to:' + user + at + server + '$subject\"$attr>$text</a>');";
 		$output .= '</script>';
 		return $output;
 	}
 
-	// --------------------------------------------------------------------------
-
 	/**
-	 * Friendly title
+	 * Converts your text to a URL-friendly title so it can be used in the URL.
 	 *
-	 * Converts your text to a URL-friendly title
-	 * so it can be used in the URL
-	 *
-	 * @access	public
-	 * @param	string	The text
-	 * @param	string	The separator (either - or _)
-	 * @return	string	The URL-friendly title
+	 * @param	string	the text
+	 * @param	string	the separator (either - or _)
+	 * @return	string	the new title
 	 */
-	public static function friendly_title($str, $sep = '-', $lowercase = FALSE)
+	public static function friendly_title($str, $sep = '-', $lowercase = false)
 	{
 		if ($sep != '-' && $sep != '_')
 		{
 			$sep = '-'; // default
 		}
 		
-        $from = array('ă','á','à','â','ã','ª','Á','À',
-          'Â','Ã', 'é','è','ê','É','È','Ê','í','ì','î','Í',
-          'Ì','Î','ò','ó','ô', 'õ','º','Ó','Ò','Ô','Õ','ş','Ş'
-          ,'ţ','Ţ','ú','ù','û','Ú','Ù','Û','ç','Ç','Ñ','ñ');
-        $to = array('a','a','a','a','a','a','A','A',
-          'A','A','e','e','e','E','E','E','i','i','i','I','I',
-          'I','o','o','o','o','o','O','O','O','O','s','S',
-          't','T','u','u','u','U','U','U','c','C','N','n');
+		$from = array(
+			'ă','á','à','â','ã','ª','Á','À',
+			'Â','Ã', 'é','è','ê','É','È','Ê','í','ì','î','Í',
+			'Ì','Î','ò','ó','ô', 'õ','º','Ó','Ò','Ô','Õ','ş','Ş',
+			'ţ','Ţ','ú','ù','û','Ú','Ù','Û','ç','Ç','Ñ','ñ'
+		);
+		$to = array(
+			'a','a','a','a','a','a','A','A',
+			'A','A','e','e','e','E','E','E','i','i','i','I','I',
+			'I','o','o','o','o','o','O','O','O','O','s','S',
+			't','T','u','u','u','U','U','U','c','C','N','n'
+		);
 		
-        $str = trim(str_replace($from, $to, $str));
+		$str = trim(str_replace($from, $to, $str));
 
-        $trans = array(
+		$trans = array(
 			'&\#\d+?;' => '',
 			'&\S+?;' => '',
 			'\s+' => $sep,
 			'[^a-zАБВГҐДЕЄЁЖЗИІЫЇЙКЛМНОПРСТУФХЦЧШЩэЮЯЬЪабвгґдеєёжзиіыїйклмнопрстуфхцчшщюяьъ0-9\-\._]' => '',
-			$sep . '+' => $sep,
-			$sep . '$' => $sep,
-			'^' . $sep => $sep,
+			$sep.'+' => $sep,
+			$sep.'$' => $sep,
+			'^'.$sep => $sep,
 			'\.+$' => ''
 		);
 
-        $str = strip_tags($str);
+		$str = App\Security::strip_tags($str);
 
-        foreach ($trans as $key => $val)
-        {
-            $str = preg_replace("#".$key."#i", $val, $str);
-        }
+		foreach ($trans as $key => $val)
+		{
+			$str = preg_replace("#".$key."#i", $val, $str);
+		}
 
-        if ($lowercase === TRUE)
-        {
-        	$str = function_exists('mb_convert_case')
+		if ($lowercase === true)
+		{
+			$str = function_exists('mb_convert_case')
 				? mb_convert_case($str, MB_CASE_LOWER, 'UTF-8')
 				: strtolower($str);
-        }
+		}
 
-        return trim(stripslashes($str));
-    }
-
-	// --------------------------------------------------------------------------
+		return trim(stripslashes($str));
+	}
 
 	/**
-	 * Prep
+	 * Adds the given schema to the given URL if it is not already there.
 	 *
-	 * Adds the http:// schema to the URL if missing
-	 *
-	 * @access	public
-	 * @param	string	The url
-	 * @return	string	The URL with http://
+	 * @param	string	the url
+	 * @param	string	the schema
+	 * @return	string	url with schema
 	 */
-	public static function prep($url)
+	public static function prep($url, $schema = 'http')
 	{
 		if ( ! preg_match('#^\w+://# i', $url))
 		{
-			$url = 'http://'.$url;
+			$url = $schema.'://'.$url;
 		}
 
 		return $url;
