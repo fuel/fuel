bd58f024f1ab9a5effd615b871fff45923693f69
Dan Horrigan <dan@dhorrigan.com>
2010-12-15 16:51:54 -0500

    Added a profiler class that uses a modified version of PHP Quick Profiler

---
+++ fuel/core/classes/profiler.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Fuel;
+
+import('phpquickprofiler/phpquickprofiler', 'vendor');
+
+use \Console;
+use \PhpQuickProfiler;
+
+class Profiler {
+	
+	protected static $profiler = null;
+	
+	public static function init()
+	{
+		static::$profiler = new PhpQuickProfiler(FUEL_START_TIME);
+	}
+	
+	public static function mark_time($label)
+	{
+		Console::logSpeed($label);
+	}
+
+	public static function mark_memory($label)
+	{
+		Console::logMemory($label);
+	}
+
+	public static function console($text)
+	{
+		Console::log($test);
+	}
+	
+	public static function output()
+	{
+		return static::$profiler->display();
+	}
+}
