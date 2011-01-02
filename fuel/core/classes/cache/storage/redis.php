7bc82fa16359041976acd96868e92f761129eb2b
Harro 'WanWizard' Verton <wanwizard@gmail.com>
2011-01-02 14:24:31 +0100

    Cache expiration is now configured in seconds instead of minutes

--- fuel/core/classes/cache/storage/redis.php
+++ fuel/core/classes/cache/storage/redis.php
@@ -130,7 +130,7 @@ class Cache_Storage_Redis extends Cache_Storage_Driver {
 		}
 
 		$this->created			= $props['created'];
-		$this->expiration		= is_null($this->expiration) ? null : (int) ($props['expiration'] - time()) / 60;
+		$this->expiration		= is_null($this->expiration) ? null : (int) ($props['expiration'] - time());
 		$this->dependencies		= $props['dependencies'];
 		$this->content_handler	= $props['content_handler'];
 	}
