5007dcb503fb017abcd0c1d3a93f246fc1504a93
Phil Sturgeon <email@philsturgeon.co.uk>
2011-01-03 18:53:07 +0000

    Updated documentation.

--- docs/assets/js/nav.js
+++ docs/assets/js/nav.js
@@ -33,12 +33,17 @@ var nav = {
 			"Cli":		"classes/cli.html",
 			"Config":	"classes/config.html",
 			"Cookie":	"classes/cookie.html",
+			"Crypt":	{
+				"Configuration":	"classes/crypt/config.html",
+				"Usage":			"classes/crypt/usage.html"
+			},
 			"Event":	"classes/event.html",
 			"Ftp":	{
 				"Configuration":	"classes/ftp/config.html",
 				"Usage":			"classes/ftp/usage.html"
 			},
 			"Input":	"classes/input.html",
+			"Log":	    "classes/log.html",
 			"Session":	{
 				"Configuration":	"classes/session/config.html",
 				"Usage":			"classes/session/usage.html",
