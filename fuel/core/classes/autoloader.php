374f988a8863b8fa3f93220ef50a160c38f007c0
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-01-11 01:17:34 +0100

    Undoing my earlier commit, caused more problems than it solved and slowed things down. For now core extensions will need to be registered in the app bootstrap.

--- fuel/core/classes/autoloader.php
+++ fuel/core/classes/autoloader.php
@@ -301,6 +301,13 @@ class Autoloader {
 			static::_init_class($class);
 			return true;
 		}
+		elseif ( ! $namespaced and $class_name = static::is_core_class($class))
+		{
+			include str_replace('/', DS, static::$classes[$class_name]);
+			static::alias_to_namespace($class_name);
+			static::_init_class($class);
+			return true;
+		}
 		elseif ( ! $namespaced)
 		{
 			$file_path = str_replace('_', DS, $class);
@@ -313,13 +320,6 @@ class Autoloader {
 				{
 					static::alias_to_namespace($class_name);
 				}
-				static::_init_class($class);
-				return true;
-			}
-			elseif ($class_name = static::is_core_class($class))
-			{
-				include str_replace('/', DS, static::$classes[$class_name]);
-				static::alias_to_namespace($class_name);
 				static::_init_class($class);
 				return true;
 			}
