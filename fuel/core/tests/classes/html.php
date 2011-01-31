f789f7e229e9374cf6d45a04da7f5dd9ca5669d5
Francesc Esplugas <francesc.esplugas@gmail.com>
2011-02-01 05:58:05 +0800

    Initial tests for the Html class

---
+++ fuel/core/tests/classes/html.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Fuel\Octane\Test;
+
+use \Fuel\Core\Html;
+use \Fuel\Octane\TestCase;
+
+class HtmlTest extends TestCase {
+
+	public function test_h()
+	{
+		$output = Html::h('Example');
+		$expected = "<h1>Example</h1>";
+		$this->assert_equal($expected, $output);
+
+		$output = Html::h('Some other example', '2', array('id' => 'h2', 'class' => 'sample', 'style' => 'color:red;'));
+		$expected = "<h2 id=\"h2\" class=\"sample\" style=\"color:red;\">Some other example</h2>";
+		$this->assert_equal($expected, $output);
+
+		$attributes = array('id' => 'sample', 'class' => 'sample', 'style' => 'color:blue;');
+		$output = Html::h('Variable!', '3', $attributes);
+		$expected = "<h3 id=\"sample\" class=\"sample\" style=\"color:blue;\">Variable!</h3>";
+		$this->assert_equal($expected, $output);
+	}
+
+	public function test_br()
+	{
+		$output = Html::br();
+		$expected = "<br />";
+		$this->assert_equal($expected, $output);
+
+		$output = Html::br('2', array('id' => 'example', 'class' => 'sample', 'style' => 'color:red;'));
+		$expected = "<br id=\"example\" class=\"sample\" style=\"color:red;\" /><br id=\"example\" class=\"sample\" style=\"color:red;\" />";
+		$this->assert_equal($expected, $output);
+	}
+
+	public function test_hr()
+	{
+		$output = Html::hr();
+		$expected = "<hr />";
+		$this->assert_equal($expected, $output);
+
+		$output = Html::hr(array('id' => 'example', 'class' => 'sample', 'style' => 'color:red;'));
+		$expected = "<hr id=\"example\" class=\"sample\" style=\"color:red;\" />";
+		$this->assert_equal($expected, $output);
+	}
+
+	public function test_title()
+	{
+		$output = Html::title();
+		$expected = "<title></title>";
+		$this->assert_equal($expected, $output);
+
+		$output = Html::title('Some Title!');
+		$expected = "<title>Some Title!</title>";
+		$this->assert_equal($expected, $output);
+	}
+
+	public function test_nbs()
+	{
+		$output = Html::nbs();
+		$expected = "&nbsp;";
+		$this->assert_equal($expected, $output);
+
+		$output = Html::nbs(2);
+		$expected = "&nbsp;&nbsp;";
+		$this->assert_equal($expected, $output);
+	}
+
+}
+
+/* End of file html.php */
