495ec98ceecc0b3ae2683e70c57035394c03d421
snrp <spam.noreply@yahoo.com>
2011-02-06 19:09:25 +0200

    Removed test code from controller welcome

--- fuel/app/classes/controller/welcome.php
+++ fuel/app/classes/controller/welcome.php
@@ -6,56 +6,7 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		//$this->render('welcome/index');
-		
-		$config = array(
-			'pagination_url' => \Uri::create('welcome/index'),
-			'total_items' => 17,
-			'per_page' => 5,
-			'uri_segment' => 3,
-		);
-	
-	/*
-		$config = array(
-			'method' => 'segment_tag',
-			
-			//'pagination_url' => , // <-- not used
-			'uri_segment' => 3, 
-
-			'uri' => 'welcome/index/:page/', // <--- notice the colon (:)
-			'get_variables' => array('foo' => 'bar' ),
-
-			// 'variable_name' => 'page', // <--- notice NO colon (:)
-			
-			// hide the segment when page_nr == 1 
-			// 'hide_1' => false, //default = true
-						
-			'total_items' => 17,
-			'per_page' => 5,
-		);
-
-		*/
-		
-		/*
-		$config = array(
-			'method' => 'get_tag',
-			
-			//'pagination_url' => '', // <-- not used
-			//'uri_segment' => 3, // <-- not used 
-			
-			'uri' => 'welcome/index', 
-			'get_variables' => array('foo' => 'bar' ),	
-			
-			// 'variable_name' => 'page', 
-					
-			'total_items' => 17,
-			'per_page' => 5,
-		
-		);
-		*/
-		
-		Pagination::set_config($config);
-		echo Pagination::create_links();
+		$this->render('welcome/index');
 	}
 
 	public function action_404()
