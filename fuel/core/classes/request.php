6d4be228e3557af6b974bd34b7d3a490a060346d
Ben Corlett <ben@tjstechnology.com.au>
2011-02-09 10:20:33 +1100

    Added Request::user_agent(). You can do certain actions from the same routes according to different properties of the user agent. \Request::user_agent('platform'); \Request::user_agent('browser'); For example: if (Request::user_agent('browser') == 'iphone') {    // load a view for the iphone only } else {    // load a view for everyone else } So your site can look different, accessed through the same url, depending on device. No need to duplicate site installations, controllers or models. Just various views (as one example).
    Signed-off-by: Ben Corlett <ben@tjstechnology.com.au>

--- fuel/core/classes/request.php
+++ fuel/core/classes/request.php
@@ -32,6 +32,11 @@ class Request {
 	 * @var	array	search paths for the current active request
 	 */
 	public $paths = array();
+	
+	/**
+	 * @var  string  client user agent
+	 */
+	public static $user_agent = '';
 
 	/**
 	 * Generates a new request.  The request is then set to be the active
@@ -253,6 +258,12 @@ class Request {
 		$this->method_params = array_slice($route['segments'], 2);
 		$this->named_params = $route['named_params'];
 
+		if (isset($_SERVER['HTTP_USER_AGENT']))
+		{
+			// Set the client user agent
+			\Request::$user_agent = $_SERVER['HTTP_USER_AGENT'];
+		}
+
 		unset($route);
 	}
 
@@ -355,6 +366,97 @@ class Request {
 	public function output()
 	{
 		echo $this->output;
+	}
+	
+	/**
+	 * User Agent
+	 * 
+	 * Returns information about the client user agent.
+	 *
+	 *     // Returns "Chrome" when using Google Chrome
+	 *     $browser = Request::user_agent('browser');
+	 *
+	 * Multiple values can be returned at once by using an array:
+	 *
+	 *     // Get the browser and platform with a single call
+	 *     $info = Request::user_agent(array('browser', 'platform'));
+	 *
+	 * When using an array for the value, an associative array will be returned.
+	 *
+	 * @param   mixed   string to return: browser, version, robot, mobile, platform; or array of values
+	 * @return  mixed   requested information, false if nothing is found
+	 * @uses    Kohana::config
+	 * @uses    Request::$user_agent
+	 */
+	public static function user_agent($value)
+	{
+		\Config::load('user_agents', true);
+		
+		if (is_array($value))
+		{
+			$agent = array();
+			
+			foreach ($value as $v)
+			{
+				// Add each key to the set
+				$agent[$v] = \Request::user_agent($v);
+			}
+
+			return $agent;
+		}
+
+		static $info;
+
+		if (isset($info[$value]))
+		{
+			// This value has already been found
+			return $info[$value];
+		}
+
+		if ($value === 'browser' OR $value == 'version')
+		{
+			// Load browsers
+			$browsers = \Config::get('user_agents.browser');
+			
+			foreach ($browsers as $search => $name)
+			{
+				if (stripos(\Request::$user_agent, $search) !== false)
+				{
+					// Set the browser name
+					$info['browser'] = $name;
+
+					if (preg_match('#'.preg_quote($search) . '[^0-9.]*+([0-9.][0-9.a-z]*)#i', Request::$user_agent, $matches))
+					{
+						// Set the version number
+						$info['version'] = $matches[1];
+					}
+					else
+					{
+						// No version number found
+						$info['version'] = false;
+					}
+
+					return $info[$value];
+				}
+			}
+		}
+		else
+		{
+			// Load the search group for this type
+			$group = \Config::get(sprintf('user_agents.%s', $value));
+
+			foreach ($group as $search => $name)
+			{
+				if (stripos(\Request::$user_agent, $search) !== false)
+				{
+					// Set the value name
+					return $info[$value] = $name;
+				}
+			}
+		}
+
+		// The value requested could not be found
+		return $info[$value] = false;
 	}
 
 	/**
