3187adc08a9ec0d13642bd5a9cd22d7f4c68d033
Dan Horrigan <dan@dhorrigan.com>
2011-03-01 14:53:08 -0500

    Migrated Html tests over to PHPUnit.

--- fuel/core/tests/HtmlTest.php
+++ fuel/core/tests/HtmlTest.php
@@ -1,81 +1,126 @@
 <?php
+/**
+ * Fuel
+ *
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package		Fuel
+ * @version		1.0
+ * @author		Fuel Development Team
+ * @license		MIT License
+ * @copyright	2010 - 2011 Fuel Development Team
+ * @link		http://fuelphp.com
+ */
 
-namespace Fuel\Octane\Test;
+namespace Fuel\Core;
 
-use \Fuel\Core\Html;
-use \Fuel\Octane\TestCase;
+/**
+ * Html class tests
+ * 
+ * @group Core
+ * @group Html
+ */
+class HtmlTest extends \PHPUnit_Framework_TestCase {
 
-class HtmlTest extends TestCase {
-
+	/**
+	 * Tests Html::h()
+	 * 
+	 * @test
+	 */
 	public function test_h()
 	{
 		$output = Html::h('Example');
 		$expected = "<h1>Example</h1>";
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 
 		$output = Html::h('Some other example', '2', array('id' => 'h2', 'class' => 'sample', 'style' => 'color:red;'));
 		$expected = '<h2 id="h2" class="sample" style="color:red;">Some other example</h2>';
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 
 		$attributes = array('id' => 'sample', 'class' => 'sample', 'style' => 'color:blue;');
 		$output = Html::h('Variable!', '3', $attributes);
 		$expected = '<h3 id="sample" class="sample" style="color:blue;">Variable!</h3>';
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 	}
 
+	/**
+	 * Tests Html::br()
+	 * 
+	 * @test
+	 */
 	public function test_br()
 	{
 		$output = Html::br();
 		$expected = "<br />";
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 
 		$output = Html::br('2', array('id' => 'example', 'class' => 'sample', 'style' => 'color:red;'));
 		$expected = '<br id="example" class="sample" style="color:red;" /><br id="example" class="sample" style="color:red;" />';
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 	}
 
+	/**
+	 * Tests Html::hr()
+	 * 
+	 * @test
+	 */
 	public function test_hr()
 	{
 		$output = Html::hr();
 		$expected = "<hr />";
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 
 		$output = Html::hr(array('id' => 'example', 'class' => 'sample', 'style' => 'color:red;'));
 		$expected = '<hr id="example" class="sample" style="color:red;" />';
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 	}
 
+	/**
+	 * Tests Html::title()
+	 * 
+	 * @test
+	 */
 	public function test_title()
 	{
 		$output = Html::title();
 		$expected = "<title></title>";
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 
 		$output = Html::title('Some Title!');
 		$expected = "<title>Some Title!</title>";
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 	}
 
+	/**
+	 * Tests Html::nbs()
+	 * 
+	 * @test
+	 */
 	public function test_nbs()
 	{
 		$output = Html::nbs();
 		$expected = "&nbsp;";
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 
 		$output = Html::nbs(2);
 		$expected = "&nbsp;&nbsp;";
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 	}
 
+	/**
+	 * Tests Html::meta()
+	 * 
+	 * @test
+	 */
 	public function test_meta()
 	{
 		$output = Html::meta('description', 'Meta Example!');
 		$expected = '<meta name="description" content="Meta Example!" />';
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 
 		$output = Html::meta('robots', 'no-cache');
 		$expected = '<meta name="robots" content="no-cache" />';
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 
 		$meta = array(
 			array('name' => 'robots', 'content' => 'no-cache'),
@@ -88,7 +133,7 @@ class HtmlTest extends TestCase {
 <meta name="robots" content="no-cache" />
 <meta name="description" content="Meta Example" />
 <meta name="keywords" content="fuel, rocks" />';
-		$this->assert_equal($expected, $output);
+		$this->assertEquals($expected, $output);
 	}
 
 }
