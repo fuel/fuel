32cb7d5cfb4580c555aa8c23072f3f9d0dc12b1e
crynobone <crynobone@gmail.com>
2011-03-09 03:13:44 +0800

    add Connector class for FuelPHP, this need some code documentation by tomorrow

---
+++ fuel/app/classes/connector.php
@@ -0,0 +1,115 @@
+<?php
+
+class Connector extends \Request {
+	
+	/**
+	 * Generates a new request.  The request is then set to be the active
+	 * request.  If this is the first request, then save that as the main
+	 * request for the app.
+	 *
+	 * Usage:
+	 *
+	 * <code>Connector::factory('GET hello/world');</code>
+	 *
+	 * @access	public
+	 * @param	string	The URI of the request
+	 * @param	bool	if true use routes to process the URI
+	 * @return	object	The new request
+	 */
+
+	public static function factory($uri, $dataset, $route = true) {
+		$uri_segments = explode(' ', $uri);
+		$type = 'GET';
+
+		if (in_array(strtoupper($uri_segments[0]), array('DELETE', 'POST', 'PUT', 'GET'))) {
+			$uri = $uri_segments[1];
+			$type = $uri_segments[0];
+		}
+
+		logger(Fuel::L_INFO, 'Creating a new Request with URI = "'.$uri.'"', __METHOD__);
+
+		static::$active = new static($uri, $route, $type, $dataset);
+
+		if ( ! static::$main)
+		{
+			logger(Fuel::L_INFO, 'Setting main Request', __METHOD__);
+			static::$main = static::$active;
+		}
+
+		return static::$active;
+	}
+	
+	public static $_request_data = array();
+	
+	public static $_request_method = '';
+	
+	public function __construct($uri, $route, $type = 'GET', $dataset = array ())
+	{
+		
+		
+		parent::__construct($uri, $route);
+		
+		static::$_request_method = $type;
+		static::$_request_data = $dataset;
+	}
+	
+	public static function input($type, $index = null, $default = null) {
+		$using_connector = (static::$_request_method !== '' ? true : false);
+		
+		/*
+		 * What if the request meant for other than get, post, get_post, put and delete?
+		 */
+		switch (strtolower($type)) {
+			case 'method' :
+				return (true === $using_connector ? strtoupper(static::$_request_method) : \Input::method());
+				break;
+			case 'is_ajax' :
+			case 'user_agent' :
+			case 'real_ip' :
+			case 'referrer' :
+				return call_user_func_array(array('\\Input', strtolower($type)));
+				break;
+		}
+		
+		/*
+		 * Reach this point but $index is null (which isn't be)
+		 */
+		if (is_null($index)) {
+			return $default;
+		}
+	
+		if (strtoupper($type) === static::$_request_method && isset(static::$_request_data[$index])) {
+			return static::$_request_data[$index];
+		}
+		else {
+			$type = strtolower($type);
+			
+			if (method_exists('\\Input', $type)) {
+				return call_user_func_array(array('\\Input', $type), array($index, $default));
+			}
+			else {
+				return $default;
+			}
+		}
+	}
+	
+	
+	/**
+	 * Cleaning up our request
+	 * 
+	 * @see \Request::execute()
+	 */
+	public function execute()
+	{
+		//$this->_request_type = '';
+		//$this->_request_dataset = array ();
+		
+		$execute = parent::execute();
+		
+		static::$_request_method = '';
+		static::$_request_data = array ();
+		
+		return $execute;
+	}
+
+}
