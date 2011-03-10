bcf4e261183028c18f5504ab83b96bc56c044f21
crynobone <crynobone@gmail.com>
2011-03-10 18:44:34 +0800

    All Hybrid development now push to branch 'hybrid'

--- fuel/packages/hybrid/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Autoloader::add_classes(array (
-	'Hybrid\\Request'		=> __DIR__.'/classes/request.php',
-	'Hybrid\\Input'			=> __DIR__.'/classes/input.php'
-));
-
-/* End of file bootstrap.php */
