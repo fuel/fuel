22f4fca0a72fdcaefb3e360beb21800207ce1739
Dan Horrigan <dan@dhorrigan.com>
2011-03-14 22:34:06 -0400

    Copy-paste strikes again.

--- fuel/app/views/welcome/404.php
+++ fuel/app/views/welcome/404.php
@@ -33,7 +33,7 @@
 
 		<p>This view can be located here:</p>
 		
-		<pre><code>APPPATH/views/welcome/index.php</code></pre>
+		<pre><code>APPPATH/views/welcome/404.php</code></pre>
 
 
 		<p class="footer">
