94acb125195bdfcbb73f9b79299bae87ebd08941
Mike Swift <theycallmeswift@gmail.com>
2011-03-24 16:59:30 -0400

    Fixed Bug #58. Scaffolding templates now use correct Html class name

--- fuel/packages/oil/views/default/scaffold/views/index.php
+++ fuel/packages/oil/views/default/scaffold/views/index.php
@@ -16,13 +16,13 @@
 <?php foreach ($fields as $field): ?>
 		<td><?php echo '<?php'; ?> echo $<?php echo $singular.'->'.$field['name']; ?>; <?php echo '?>'; ?></td>
 <?php endforeach; ?>
-		<td><?php echo '<?php'; ?> echo HTML::anchor('<?php echo $plural; ?>/view/'.$<?php echo $singular; ?>->id, 'View'); <?php echo '?>'; ?></td>
-		<td><?php echo '<?php'; ?> echo HTML::anchor('<?php echo $plural; ?>/edit/'.$<?php echo $singular; ?>->id, 'Edit'); <?php echo '?>'; ?></td>
-		<td><?php echo '<?php'; ?> echo HTML::anchor('<?php echo $plural; ?>/delete/'.$<?php echo $singular; ?>->id, 'Delete', array('onclick' => "return confirm('Are you sure?')")); <?php echo '?>'; ?></td>
+		<td><?php echo '<?php'; ?> echo Html::anchor('<?php echo $plural; ?>/view/'.$<?php echo $singular; ?>->id, 'View'); <?php echo '?>'; ?></td>
+		<td><?php echo '<?php'; ?> echo Html::anchor('<?php echo $plural; ?>/edit/'.$<?php echo $singular; ?>->id, 'Edit'); <?php echo '?>'; ?></td>
+		<td><?php echo '<?php'; ?> echo Html::anchor('<?php echo $plural; ?>/delete/'.$<?php echo $singular; ?>->id, 'Delete', array('onclick' => "return confirm('Are you sure?')")); <?php echo '?>'; ?></td>
 	</tr>
 	<?php echo '<?php endforeach; ?>'; ?>
 </table>
 
 <br />
 
-<?php echo '<?php'; ?> echo HTML::anchor('<?php echo $plural; ?>/create', 'Add new <?php echo \Inflector::humanize($singular); ?>'); <?php echo '?>'; ?>
+<?php echo '<?php'; ?> echo Html::anchor('<?php echo $plural; ?>/create', 'Add new <?php echo \Inflector::humanize($singular); ?>'); <?php echo '?>'; ?>
