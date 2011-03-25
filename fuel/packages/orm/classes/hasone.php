41e2736589baf741777698408607cdf7c0bda0ef
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-03-25 18:14:47 +0100

    ORM: a beginning for implementing saving relationships and cascading save/delete.

--- fuel/packages/orm/classes/hasone.php
+++ fuel/packages/orm/classes/hasone.php
@@ -24,6 +24,9 @@ class HasOne extends Relation {
 		$this->model_to    = array_key_exists('model_to', $config) ? $config['model_to'] : 'Model_'.\Inflector::classify($name);
 		$this->key_from    = array_key_exists('key_from', $config) ? (array) $config['key_from'] : $this->key_from;
 		$this->key_to      = array_key_exists('key_to', $config) ? (array) $config['key_to'] : (array) \Inflector::foreign_key($this->model_from);
+
+		$this->cascade_save    = array_key_exists('cascade_save', $config) ? $config['cascade_save'] : $this->cascade_save;
+		$this->cascade_delete  = array_key_exists('cascade_save', $config) ? $config['cascade_save'] : $this->cascade_delete;
 	}
 
 	public function get(Model $from)
