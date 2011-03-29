1fbf0e4efbd7f96800da9fda7a34fb488cff70a1
Dan Horrigan <dan@dhorrigan.com>
2011-03-29 19:34:08 -0400

    Added DocBlocks to the Welcome controller.

--- fuel/app/classes/controller/welcome.php
+++ fuel/app/classes/controller/welcome.php
@@ -1,14 +1,33 @@
 <?php
+
 /**
- * An example Controller.  This shows the most basic usage of a Controller.
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ * 
+ * @package  app
+ * @extends  Controller
  */
 class Controller_Welcome extends Controller {
 
+	/**
+	 * The index action.
+	 * 
+	 * @access  public
+	 * @return  void
+	 */
 	public function action_index()
 	{
 		$this->response->body = View::factory('welcome/index');
 	}
 
+	/**
+	 * The 404 action for the application.
+	 * 
+	 * @access  public
+	 * @return  void
+	 */
 	public function action_404()
 	{
 		$messages = array('Aw, crap!', 'Bloody Hell!', 'Uh Oh!', 'Nope, not here.', 'Huh?');
@@ -19,3 +38,5 @@ class Controller_Welcome extends Controller {
 		$this->response->body = View::factory('welcome/404', $data);
 	}
 }
+
+/* End of file welcome.php */
