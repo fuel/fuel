b6c2685109cd313661c1deb911abd95c0b310983
barrymieny <github@barrymieny.com>
2011-04-01 03:51:37 -0700

    Fixed incorrect quote marks in CSS

--- fuel/packages/oil/views/default/template.php
+++ fuel/packages/oil/views/default/template.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title><?php echo $title; ?></title>
 	<style type="text/css">
-		body { background-color: #F2F2F2; margin: 45px 0 0 0; font-family: ‘Palatino Linotype’, ‘Book Antiqua’, Palatino, serif; font-size: 18px }
+		body { background-color: #F2F2F2; margin: 45px 0 0 0; font-family: 'Palatino Linotype', 'Book Antiqua', Palatino, serif; font-size: 18px }
 		#wrapper { width: 740px; margin: 0 auto; }
 		h1 { color: #333333; font: normal normal normal 62px/1em Impact, Charcoal, sans-serif; margin: 0 0 15px 0; }
 		pre { padding: 15px; background-color: #FFF; border: 1px solid #CCC; font-size: 16px;}
