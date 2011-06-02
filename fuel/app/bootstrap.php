de54ca831b9285c6d07f04fd45dd6b8aa64086a9
FrenkyNet <info@frenky.net>
2011-06-03 00:27:26 +0200

    Removed date_default_timezone_set, was allready set in Fuel::init

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -3,16 +3,6 @@
 // Bootstrap the framework DO NOT edit this
 require_once COREPATH.'bootstrap.php';
 
-/**
- * Set the timezone to what you need it to be.
- */
-date_default_timezone_set('UTC');
-
-/**
- * Set the encoding you would like to use.
- */
-Fuel::$encoding = 'UTF-8';
-
 
 Autoloader::add_classes(array(
 	// Add classes you want to override here
