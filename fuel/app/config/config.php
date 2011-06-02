d5605fa59fc5e48a60f5f3dff5026de0b84d2c1a
unknown <hverton@.(none)>
2011-06-02 17:55:46 +0200

    Cookie class is now configured through app/config/config.php. Closes #170.

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -114,6 +114,37 @@ return array(
 	),
 
 	/**
+	 * Cookie settings
+	 */
+	'cookie' => array(
+
+		/**
+		 * Number of seconds before the cookie expires
+		 */
+		'expiration'            => 0,
+
+		/**
+		 * Restrict the path that the cookie is available to
+		 */
+		'path'                  => '/',
+
+		/**
+		 * Restrict the domain that the cookie is available to
+		 */
+		'domain'                => null,
+
+		/**
+		 * Only transmit cookies over secure connections
+		 */
+		'secure'                => false,
+
+		/**
+		 * Only transmit cookies over HTTP, disabling Javascript access
+		 */
+		'http_only'             => false,
+	),
+
+	/**
 	 * To enable you to split up your application into modules which can be
 	 * routed by the first uri segment you have to define their basepaths
 	 * here. By default empty, but to use them you can add something
