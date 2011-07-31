6cb166bc9b7f814f4c7c58c4af71c12857c92bae
Dan Horrigan <dan@dhorrigan.com>
2011-07-31 18:53:59 -0400

    Changed the default logging level to L_WARNING.

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -95,7 +95,7 @@ return array(
 	 * Fuel::L_INFO
 	 * Fuel::L_ALL
 	 */
-	'log_threshold'		=> Fuel::L_ERROR,
+	'log_threshold'		=> Fuel::L_WARNING,
 	'log_path'			=> APPPATH.'logs/',
 	'log_date_format' 	=> 'Y-m-d H:i:s',
 
