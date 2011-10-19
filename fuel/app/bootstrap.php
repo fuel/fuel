1e93d9b56a39281798e9fef58da64d7c8d9fb831
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2011-10-19 18:25:37 +0200

    Autoloader loaded in app bootstrap to allow extending it.

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+// Load in the Autoloader
+require COREPATH.'classes'.DIRECTORY_SEPARATOR.'autoloader.php';
+class_alias('Fuel\\Core\\Autoloader', 'Autoloader');
+
 // Bootstrap the framework DO NOT edit this
 require COREPATH.'bootstrap.php';
 
