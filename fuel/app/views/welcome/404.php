e7e33fb7e6e924aed42fa5479b7608f5f554fda0
Dan Horrigan <dan@dhorrigan.com>
2011-10-26 00:25:53 -0700

    Adding the Twitter bootstrap and using it.  Looks nicer.

--- fuel/app/views/welcome/404.php
+++ fuel/app/views/welcome/404.php
@@ -3,42 +3,28 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php echo e($title); ?></title>
-	<style type="text/css">
-		* { margin: 0; padding: 0; }
-		body { background-color: #EEE; font-family: sans-serif; font-size: 16px; line-height: 20px; margin: 40px; }
-		#wrapper { padding: 30px; background: #fff; color: #333; margin: 0 auto; width: 600px; }
-		a { color: #36428D; }
-		h1 { color: #000; font-size: 55px; padding: 0 0 25px; line-height: 1em; }
-		.intro { font-size: 22px; line-height: 30px; font-family: georgia, serif; color: #555; padding: 29px 0 20px; border-top: 1px solid #CCC; }
-		h2 { margin: 50px 0 15px; padding: 0 0 10px; font-size: 18px; border-bottom: 1px dashed #ccc; }
-		h2.first { margin: 10px 0 15px; }
-		p { margin: 0 0 15px; line-height: 22px;}
-		a { color: #666; }
-		pre { border-left: 1px solid #ddd; line-height:20px; margin:20px; padding-left:1em; font-size: 16px; }
-		pre, code { color:#137F80; font-family: Courier, monospace; }
-		ul { margin: 15px 30px; }
-		li { line-height: 24px;}
-		.footer { color: #777; font-size: 12px; margin: 40px 0 0 0; }
+	<?php echo Asset::css('bootstrap.css'); ?>
+	<style>
+		body { margin: 40px; }
 	</style>
 </head>
 <body>
-	<div id="wrapper">
-		<h1><?php echo e($title); ?></h1>
-		
-		<p class="intro">The page you are looking for cannot be found.</p>
-		
-		<p>The controller that is generating this page is located here:</p>
-
-		<pre><code>APPPATH/classes/controller/welcome.php</code></pre>
-
-		<p>This view can be located here:</p>
-		
-		<pre><code>APPPATH/views/welcome/404.php</code></pre>
-
-
-		<p class="footer">
-			<a href="http://fuelphp.com">Fuel</a> is released under the MIT license.<br />Page rendered in {exec_time}s using {mem_usage}mb of memory.
-		</p>
+	<div class="container">
+		<div class="row">
+			<div class="span16">
+				<h1><?php echo e($title); ?> <small>We can't find that!</small></h1>
+				<hr>
+				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
+				<p>This view is located at <code>APPPATH/views/welcome/404.php</code>.</p>
+			</div>
+		</div>
+		<footer>
+			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
+			<p>
+				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
+				<small>Version: <?php echo e(Fuel::VERSION); ?></small>
+			</p>
+		</footer>
 	</div>
 </body>
 </html>
--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -2,55 +2,48 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Fuel PHP Framework</title>
-	<style type="text/css">
-		* { margin: 0; padding: 0; }
-		body { background-color: #EEE; font-family: sans-serif; font-size: 16px; line-height: 20px; margin: 40px; }
-		#wrapper { padding: 30px; background: #fff; color: #333; margin: 0 auto; width: 600px; }
-		a { color: #36428D; }
-		h1 { color: #000; font-size: 55px; padding: 0 0 25px; line-height: 1em; }
-		.intro { font-size: 22px; line-height: 30px; font-family: georgia, serif; color: #555; padding: 29px 0 20px; border-top: 1px solid #CCC; }
-		h2 { margin: 50px 0 15px; padding: 0 0 10px; font-size: 18px; border-bottom: 1px dashed #ccc; }
-		h2.first { margin: 10px 0 15px; }
-		p { margin: 0 0 15px; line-height: 22px;}
-		a { color: #666; }
-		pre { border-left: 1px solid #ddd; line-height:20px; margin:20px; padding-left:1em; font-size: 16px; }
-		pre, code { color:#137F80; font-family: Courier, monospace; }
-		ul { margin: 15px 30px; }
-		li { line-height: 24px;}
-		.footer { color: #777; font-size: 12px; margin: 40px 0 0 0; }
+	<title>FuelPHP Framework</title>
+	<?php echo Asset::css('bootstrap.css'); ?>
+	<style>
+		body { margin: 40px; }
 	</style>
 </head>
 <body>
-	<div id="wrapper">
-		<h1>Fuel</h1>
-		
-		<p class="intro">You have successfully installed the Fuel PHP Framework.</p>
-		
-		<h2 class="first">Version: <?php echo e(Fuel::VERSION); ?></h2>
-		
-		<p>The controller that is generating this page is located here:</p>
-
-		<pre><code>APPPATH/classes/controller/welcome.php</code></pre>
-
-		<p>This view can be located here:</p>
-		
-		<pre><code>APPPATH/views/welcome/index.php</code></pre>
-
-		<h2>Now What?</h2>
-
-		<p>Now that you have Fuel installed, here are a few links you might find useful:</p>
-		
-		<ul>
-			<li><a href="http://fuelphp.com/docs" target="_blank">Documentation</a></li>
-			<li><a href="http://fuelphp.com/" target="_blank">Official Website</a></li>
-			<li><a href="http://github.com/fuel/fuel" target="_blank">GitHub Respository</a></li>
-			<li><a href="http://fuelphp.com/contribute/issue-tracker" target="_blank">Issue Tracker</a></li>
-		</ul>
-
-		<p class="footer">
-			<a href="http://fuelphp.com">Fuel</a> is released under the MIT license.<br />Page rendered in {exec_time}s using {mem_usage}mb of memory.
-		</p>
+	<div class="container">
+		<div class="hero-unit">
+			<h1>Welcome!</h1>
+			<p>You have successfully installed the FuelPHP Framework.</p>
+			<p><a class="btn primary large" href="http://fuelphp.com/docs">Read the Docs</a></p>
+		</div>
+		<div class="row">
+			<div class="span-one-third">
+				<h2>Get Started</h2>
+				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
+				<p>This view can be found at <code>APPPATH/views/welcome/index.php</code>.</p>
+				<p>You can modify these files to get your application started quickly.</p>
+			</div>
+			<div class="span-one-third">
+				<h2>Learn</h2>
+				<p>The best way to learn FuelPHP is reading through the <a href="http://fuelphp.com/docs">Documentation</a>.</p>
+				<p>Another good resource is the <a href="http://fuelphp.com/forums">Forums</a>.  They are fairly active, and you can usually get a response quickly.</p>
+			</div>
+			<div class="span-one-third">
+				<h2>Contribute</h2>
+				<p>FuelPHP wouldn't exist without awesome contributions from the community.  Use the links below to get contributing.</p>
+				<ul>
+					<li><a href="http://fuelphp.com/docs/general/coding_standards.html">Coding Standards</a></li>
+					<li><a href="http://github.com/fuel/fuel">GitHub Respository</a></li>
+					<li><a href="http://fuelphp.com/contribute/issue-tracker">Issue Tracker</a></li>
+				</ul>
+			</div>
+		</div>
+		<footer>
+			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
+			<p>
+				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
+				<small>Version: <?php echo e(Fuel::VERSION); ?></small>
+			</p>
+		</footer>
 	</div>
 </body>
 </html>
