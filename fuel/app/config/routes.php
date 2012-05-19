aab031bdb29a9cc4e16b657bf47cf3ca7beff20e
Thomas Edwards <thomas.edwards@gmail.com>
2012-05-19 18:03:30 +0100

    Replaced spaces with tabs

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
-	'_404_'   => 'welcome/404',    // The main 404 route
+	'_root_'	=> 'welcome/index',	// The default route
+	'_404_'		=> 'welcome/404',	// The main 404 route
 	
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
