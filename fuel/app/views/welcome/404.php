491c96e3accadbf10dfad8df08bf15986c7d1981
WanWizard <wanwizard@wanwizard.eu>
2012-10-26 23:37:17 +0200

    removed e() calls in the welcome views.
    these are not needed, everything is encoded by default.

--- fuel/app/views/welcome/404.php
+++ fuel/app/views/welcome/404.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo e($title); ?></title>
+	<title><?php echo $title; ?></title>
 	<?php echo Asset::css('bootstrap.css'); ?>
 	<style>
 		#logo{
@@ -42,7 +42,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="span16">
-				<h1><?php echo e($title); ?> <small>We can't find that!</small></h1>
+				<h1><?php echo $title; ?> <small>We can't find that!</small></h1>
 				<hr>
 				<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
 				<p>This view is located at <code>APPPATH/views/welcome/404.php</code>.</p>
@@ -52,7 +52,7 @@
 			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
 			<p>
 				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
-				<small>Version: <?php echo e(Fuel::VERSION); ?></small>
+				<small>Version: <?php echo Fuel::VERSION; ?></small>
 			</p>
 		</footer>
 	</div>
--- fuel/app/views/welcome/hello.php
+++ fuel/app/views/welcome/hello.php
@@ -53,7 +53,7 @@
 			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
 			<p>
 				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
-				<small>Version: <?php echo e(Fuel::VERSION); ?></small>
+				<small>Version: <?php echo Fuel::VERSION; ?></small>
 			</p>
 		</footer>
 	</div>
