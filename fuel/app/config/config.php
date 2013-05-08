2491475c4a2f27597bc9ad953de6caea72b3c0ab
WanWizard <wanwizard@wanwizard.eu>
2013-05-08 22:18:48 +0200

    Bumped the docblock version to 1.7

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -3,7 +3,7 @@
  * Part of the Fuel framework.
  *
  * @package    Fuel
- * @version    1.6
+ * @version    1.7
  * @author     Fuel Development Team
  * @license    MIT License
  * @copyright  2010 - 2013 Fuel Development Team
--- fuel/app/tasks/robots.php
+++ fuel/app/tasks/robots.php
@@ -3,7 +3,7 @@
  * Fuel is a fast, lightweight, community driven PHP5 framework.
  *
  * @package    Fuel
- * @version    1.6
+ * @version    1.7
  * @author     Fuel Development Team
  * @license    MIT License
  * @copyright  2010 - 2013 Fuel Development Team
