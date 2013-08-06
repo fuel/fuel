8f01fa59346bd8a3303815124c85f9976a14d3d2
Axel Pardemann <axelitusdev@gmail.com>
2013-08-06 15:36:52 -0500

    All Exceptions thath are not handled, are now handled by a 500 handler which displays a 500 error page when the environment is set to production. For all other environments the Exceptions remain unhandled.

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -2,6 +2,7 @@
 return array(
 	'_root_'  => 'welcome/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
+    '_500_'   => 'welcome/500',    // The main 500 route
 	
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
