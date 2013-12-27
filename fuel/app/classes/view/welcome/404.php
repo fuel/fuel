332a22c75dd62a6c1e6e1a39acf3b91d008cf346
WanWizard <wanwizard@wanwizard.eu>
2013-12-27 12:56:53 +0100

    changed the copyright to 2014

--- fuel/app/classes/view/welcome/404.php
+++ fuel/app/classes/view/welcome/404.php
@@ -6,7 +6,7 @@
  * @version    1.7
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2013 Fuel Development Team
+ * @copyright  2010 - 2014 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
