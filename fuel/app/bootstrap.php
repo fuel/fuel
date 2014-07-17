1d47e2b253410ebbfdc22fefa8660a009e4ef119
Michiel Vugteveen <info@it-can.nl>
2014-07-17 09:43:02 +0200

    fix global namespaces

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -3,13 +3,13 @@
 require COREPATH.'bootstrap.php';
 
 
-Autoloader::add_classes(array(
+\Autoloader::add_classes(array(
 	// Add classes you want to override here
 	// Example: 'View' => APPPATH.'classes/view.php',
 ));
 
 // Register the autoloader
-Autoloader::register();
+\Autoloader::register();
 
 /**
  * Your environment.  Can be set to any of the following:
@@ -19,7 +19,7 @@ Autoloader::register();
  * Fuel::STAGING
  * Fuel::PRODUCTION
  */
-Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : Fuel::DEVELOPMENT);
+\Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : \Fuel::DEVELOPMENT);
 
 // Initialize the framework with the config file.
-Fuel::init('config.php');
+\Fuel::init('config.php');
