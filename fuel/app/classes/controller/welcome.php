13cae8560d404488d105934a1ed837ee5037b28a
WanWizard <wanwizard@wanwizard.eu>
2015-01-22 11:08:00 +0100

    bumped the copyright to 2015

--- fuel/app/classes/controller/welcome.php
+++ fuel/app/classes/controller/welcome.php
@@ -6,7 +6,7 @@
  * @version    1.7
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2014 Fuel Development Team
+ * @copyright  2010 - 2015 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
