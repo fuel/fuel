bc547621b05edb8aa9ac27c33afc17efb38e9879
WanWizard <wanwizard@wanwizard.eu>
2015-05-19 22:58:45 +0100

    added exception catching in the front controller
    and besides 404's, now 403's and 500's are also caught, if a route
    exists

--- public/index.php
+++ public/index.php
@@ -46,50 +46,70 @@ if ( ! file_exists(COREPATH.'classes'.DIRECTORY_SEPARATOR.'autoloader.php'))
 	die('No composer autoloader found. Please run composer to install the FuelPHP framework dependencies first!');
 }
 
+// Activate the framework class autoloader
 require COREPATH.'classes'.DIRECTORY_SEPARATOR.'autoloader.php';
 class_alias('Fuel\\Core\\Autoloader', 'Autoloader');
 
-// Boot the app
-require APPPATH.'bootstrap.php';
-
-// Generate the request, execute it and send the output.
-try
+// Exception route processing closure
+$routerequest = function($route = null, $e = false)
 {
-	$response = \Request::forge()->execute()->response();
-}
-catch (\HttpNotFoundException $e)
-{
-	\Request::reset_request(true);
+	Request::reset_request(true);
 
-	$route = array_key_exists('_404_', \Router::$routes) ? \Router::$routes['_404_']->translation : \Config::get('routes._404_');
+	$route = array_key_exists($route, Router::$routes) ? Router::$routes[$route]->translation : Config::get('routes.'.$route);
 
 	if ($route instanceof Closure)
 	{
 		$response = $route();
 
-		if( ! $response instanceof \Response)
+		if( ! $response instanceof Response)
 		{
-			$response = \Response::forge($response);
+			$response = Response::forge($response);
 		}
+	}
+	elseif ($e === false)
+	{
+		$response = Request::forge()->execute()->response();
 	}
 	elseif ($route)
 	{
-		$response = \Request::forge($route, false)->execute()->response();
+		$response = Request::forge($route, false)->execute(array($e))->response();
 	}
 	else
 	{
 		throw $e;
 	}
-}
 
-// Render the output
-$response->body((string) $response);
+	return $response;
+};
+
+// Generate the request, execute it and send the output.
+try
+{
+	// Boot the app...
+	require APPPATH.'bootstrap.php';
+
+	// ... and execute the main request
+	$response = $routerequest();
+}
+catch (HttpNoAccessException $e)
+{
+	$response = $routerequest('_403_', $e);
+}
+catch (HttpNotFoundException $e)
+{
+	$response = $routerequest('_404_', $e);
+}
+catch (HttpServerErrorException $e)
+{
+	$response = $routerequest('_500_', $e);
+}
 
 // This will add the execution time and memory usage to the output.
 // Comment this out if you don't use it.
+$response->body((string) $response);
 if (strpos($response->body(), '{exec_time}') !== false or strpos($response->body(), '{mem_usage}') !== false)
 {
-	$bm = \Profiler::app_total();
+	$bm = Profiler::app_total();
 	$response->body(
 		str_replace(
 			array('{exec_time}', '{mem_usage}'),
@@ -99,4 +119,5 @@ if (strpos($response->body(), '{exec_time}') !== false or strpos($response->body
 	);
 }
 
+// Send the output to the client
 $response->send(true);
