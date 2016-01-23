d45b0343c2a30165d02e6429a1c8bf02c4dcd38f
WanWizard <wanwizard@wanwizard.eu>
2016-01-23 12:37:08 +0000

    Bumped the copyright to 2016, the version to 1.8

--- fuel/app/classes/controller/welcome.php
+++ fuel/app/classes/controller/welcome.php
@@ -3,10 +3,10 @@
  * Fuel is a fast, lightweight, community driven PHP5 framework.
  *
  * @package    Fuel
- * @version    1.7
+ * @version    1.8
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2015 Fuel Development Team
+ * @copyright  2010 - 2016 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -3,10 +3,10 @@
  * Part of the Fuel framework.
  *
  * @package    Fuel
- * @version    1.7
+ * @version    1.8
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2015 Fuel Development Team
+ * @copyright  2010 - 2016 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
