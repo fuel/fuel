bbe0688897eec1c80dcd66d14e12031bde40c59e
WanWizard <wanwizard@wanwizard.eu>
2021-04-10 21:02:36 +0100

    Correct framework name; add PHP version to the welcome page

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -38,7 +38,7 @@
 	<div class="container">
 		<div class="jumbotron">
 			<h1>Welcome!</h1>
-			<p>You have successfully installed the FuelPHP Framework.</p>
+			<p>You have successfully installed the Fuel PHP Framework.</p>
 			<p><a class="btn btn-primary btn-lg" href="https://docs.fuelphp.com">Read the Docs</a></p>
 		</div>
 		<div class="row">
@@ -50,12 +50,12 @@
 			</div>
 			<div class="col-md-4">
 				<h2>Learn</h2>
-				<p>The best way to learn FuelPHP is reading through the <a href="https://docs.fuelphp.com">Documentation</a>.</p>
+				<p>The best way to learn Fuel is reading through the <a href="https://docs.fuelphp.com">Documentation</a>.</p>
 				<p>Another good resource is the <a href="https://forums.fuelphp.com">Forums</a>.  They are fairly active, and you can usually get a response quickly.</p>
 			</div>
 			<div class="col-md-4">
 				<h2>Contribute</h2>
-				<p>FuelPHP wouldn't exist without awesome contributions from the community.  Use the links below to get contributing.</p>
+				<p>Fuel wouldn't exist without awesome contributions from the community.  Use the links below to get contributing.</p>
 				<ul>
 					<li><a href="https://docs.fuelphp.com/general/coding_standards.html">Coding Standards</a></li>
 					<li><a href="https://github.com/fuel/fuel">GitHub Respository</a></li>
@@ -67,8 +67,8 @@
 		<footer>
 			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
 			<p>
-				<a href="https://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
-				<small>Version: <?php echo Fuel::VERSION; ?></small>
+				<a href="https://fuelphp.com">Fuel</a> PHP Framework is released under the MIT license.<br>
+				<small>Fuel v<?php echo Fuel::VERSION;?> running on PHP v<?php echo phpversion();?></small>
 			</p>
 		</footer>
 	</div>
