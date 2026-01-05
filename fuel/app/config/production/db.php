55b7ea6dde7dbbe09b6ac5291a692663c1ce0cab
WanWizard <wanwizard@wanwizard.eu>
2026-01-05 19:55:01 +0000

    Update the copyright to 2026

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,7 +6,7 @@
  * @version    1.9-dev
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010-2025 Fuel Development Team
+ * @copyright  2010-2026 Fuel Development Team
  * @link       https://fuelphp.com
  */
 
